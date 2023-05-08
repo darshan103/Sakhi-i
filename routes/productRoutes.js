21a0c7e31af118c1d8216e3a953e5f08f14ea449
Darshan Singh <singhdarshan2166@gmail.com>
2023-05-08 18:31:08 +0530

    added-filters-Sakhi

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js"
-import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController } from "../controllers/productController.js"
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productCategoryController, productCountController, productFiltersController, productListController, productPhotoController, realtedProductController, searchProductController, updateProductController } from "../controllers/productController.js"
 import formidable from 'express-formidable'
 
 const router = express.Router()
@@ -22,6 +22,24 @@ router.get('/product-photo/:pid', productPhotoController);
 router.delete('/delete-product/:pid', deleteProductController);
 
 // update product
-router.post('/update-product/:pid', requireSignIn, isAdmin, formidable(), updateProductController)
+router.put('/update-product/:pid', requireSignIn, isAdmin, formidable(), updateProductController)
+
+// filter product
+router.post('/product-filters', productFiltersController)
+
+// product count
+router.get('/product-count', productCountController)
+
+// product per page
+router.get('/product-list/:page', productListController)
+
+// search product
+router.get('/search/:keyword', searchProductController)
+
+//similar product
+router.get("/related-product/:pid/:cid", realtedProductController);
+
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
 
 export default router
